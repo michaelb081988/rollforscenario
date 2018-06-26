6a8ee3f01ceec194aecabf6cc4eb81aa034ae901
michaelb081988 <michaelb081988@gmail.com>
2018-06-26 12:43:35 +0800

    Final 2017 steamroller update
    Fixed inconsistant filename/links on pages and made changes to make it simpler to update to SR2018/all future updates.

--- js/rollforscenario-2017.js
+++ js/rollforscenario-2017.js
@@ -1,13 +1,15 @@
 	var ranNum = 0; // The random scenario number
 	var lasRanNum = 0; // The last scenario, used to skip re-rolling into the same one.
 	
-	// This will become the json string we read
+	var numberOfScenarios = 6; // Current number of scenarios
+	
+	// This will become the JSON object we read
 	var j = {};	
 
 	// This gets us a random number that is NOT the same as the old one. Multiply Math.random by the number of scenarios available and add 1 so we don't get 0.
 	function randomNumber() {
 		if(lasRanNum == ranNum) {
-			ranNum = Math.floor(Math.random()*6) +1;
+			ranNum = Math.floor(Math.random() * numberOfScenarios) +1;
 		}
 		lasRanNum = ranNum;
 		console.log(ranNum);
@@ -16,7 +18,7 @@
 	
 	// Get the scenario from the JSON object.
 	function getScenario() {
-		num = ranNum -1; //Get the json object number, random number -1 as the array is 0 indexed.
+		num = ranNum -1; //Get the JSON object number, random number -1 as the array is 0 indexed.
 		var scen = j.scenario[num]; //Get the scenario here
 		
 		//Assign the scenario strings to the various HTML areas on the page.
@@ -32,7 +34,10 @@
 	function rollScenario() {
 		randomNumber();
 		getScenario();
-		ga('send', 'event', 'Scenario', 'Random', $('#scenario-name').text()); // Used for Google analytics. Lets me view how often scenarios come up.
+		
+		// Used for Google analytics.
+		// Lets me view how often scenarios come up.
+		ga('send', 'event', 'Scenario', 'Random', $('#scenario-name').text());
 	}
 	
 	// The reroll scenario button at the top of the page. Empties some of the screen text to cleanly randomise a new scenario.
@@ -50,26 +55,35 @@
 		ga('send', 'event', 'Scenario', 'Selected', $('#scenario-name').text());
 	}
 	
-	// Dropdown box for the Objectives.
+	// Adds the loaded scenarios into the Choose Scenario dropdown
+	function populateChooseScenario() {
+		$.each(j.scenario, function(i, item) {
+			$('#scenario').append($('<option>', {
+				value: item.id,
+				text: item.title
+			}));
+		});
+	}
 	
-	$(document).on('change', '#objective1', function() {
-		$("#objective1-image").attr("src","img/objective"+$("#objective1")[0].selectedIndex+"-2017.jpg");
-	});
+	// Counts the objects in the JSON array. Probably a lazy way to do this but it works.
+	function getNumberOfScenarios() {
+		numberOfScenarios = 0;
+		$.each(j.scenario, function(i, item) {
+			numberOfScenarios++;
+		});
+	}
 	
 	// Calls chooseScenario() when the dropdown box is changed
 	$(document).on('change', '#scenario', function() {
 		chooseScenario($('#scenario')[0].selectedIndex);
 		});
-		
-	// Outdated, not in use, but still funny to me.
-	$('.funnybutton').click( function() {
-		$('body').addClass("comic");
-		ga('send', 'event', 'Funny', 'Funny', 'Funny');
-	});
 
-	//THE LONG JSON string. This is all the scenarios in a single JSON object. We get this from the file in the json folder.
+	// THE LONG JSON string. This is all the scenarios in a single JSON object. 
+	// We get this from the file in the JSON folder.
 	var jqxhr = $.getJSON( "json/scenarios-2017.json", function() {})
 		.done(function(data) {
-			j = data; // Load the json data from the json file into the j variable declared at the top.
-			rollScenario();// Aaaand begin. (this runs after the json file is loaded...)
+			j = data; // Load the JSON data from the JSON file into the j variable declared at the top.
+			getNumberOfScenarios();
+			populateChooseScenario();
+			rollScenario();// Aaaand begin. (this runs after the JSON file is loaded...)
 		});
